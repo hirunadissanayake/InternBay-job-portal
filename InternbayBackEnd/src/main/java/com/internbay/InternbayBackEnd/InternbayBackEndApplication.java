0280bb8aa6376e564a445892ab39bb6146863c99
hirunadissanyake <hirunasankalpa@gmail.com>
2025-08-29 02:03:10 +0530

    edit profile

---
+++ InternbayBackEnd/src/main/java/com/internbay/InternbayBackEnd/InternbayBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.internbay.InternbayBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternbayBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternbayBackEndApplication.class, args);
+	}
+
+}
---
+++ InternbayBackEnd/src/test/java/com/internbay/InternbayBackEnd/InternbayBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internbay.InternbayBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternbayBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
