88f81ef962243a89eec5b019ac43976c4e88816b
hirunadissanyake <hirunasankalpa@gmail.com>
2025-08-29 09:23:58 +0530

    add backend structure

---
+++ internbayBackEnd/src/main/java/lk/ijse/gdse/main/internbaybackend/InternbayBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.main.internbaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternbayBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternbayBackEndApplication.class, args);
+    }
+
+}
---
+++ internbayBackEnd/src/test/java/lk/ijse/gdse/main/internbaybackend/InternbayBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.main.internbaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternbayBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
